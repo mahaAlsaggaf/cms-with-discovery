864da2878260b47a31c2127d278a64b1340b6b77
Maha <mahaAlsaggafx@gmail.con>
2025-08-24 08:03:24 +0300

    Generate the Episodes resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SeriesModule } from './series/series.module';
+import { EpisodesModule } from './episodes/episodes.module';
 
 @Module({
-  imports: [SeriesModule],
+  imports: [SeriesModule, EpisodesModule],
   controllers: [AppController],
   providers: [AppService],
 })

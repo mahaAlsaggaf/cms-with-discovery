028fae92cc2582c3131830099a236b6a80938dee
Maha <mahaAlsaggafx@gmail.con>
2025-08-24 16:57:54 +0300

    Create episode entity

--- src/episodes/entities/episode.entity.ts
+++ src/episodes/entities/episode.entity.ts
@@ -1,1 +1,50 @@
-export class Episode {}
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, BeforeUpdate } from 'typeorm';
+import { Series } from '../../series/entities/series.entity';
+
+@Entity('episode')
+export class Episode {
+
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column({default: ''})
+  description: string;
+
+  @Column()
+  videoUrl: string;
+
+  @Column({default: ''})
+  thumbnailUrl: string;
+
+  @Column({default: 0})
+  duration: number; // in seconds
+
+  @Column()
+  episodeNumber: number;
+
+  @Column({nullable: true})
+  seasonNumber: number;
+
+  @Column({default: false})
+  isPublished: boolean;
+
+  @Column({ type: 'timestamp', nullable: true })
+  publishedAt: Date;
+
+  @Column({ type: 'timestamp', default: () => "CURRENT_TIMESTAMP"})
+  createdAt: Date;
+
+  @Column({ type: 'timestamp', default: () => "CURRENT_TIMESTAMP"})
+  updatedAt: Date;
+
+  @BeforeUpdate()
+  updateTimestamp() {
+    this.updatedAt = new Date();
+  }
+
+  @ManyToOne(() => Series, series => series.episodes)
+  series: Series;
+}
